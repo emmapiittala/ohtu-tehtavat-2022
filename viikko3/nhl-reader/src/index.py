efe0b519e563b897d85debfef3546b914b056cef
Emma Piittala <emma.piittala@gmail.com>
2022-03-28 17:42:48 +0300

    pushataa index uusiks

--- viikko3/nhl-reader/src/index.py
+++ viikko3/nhl-reader/src/index.py
@@ -12,39 +12,7 @@ def main():
     stats = PlayerStats(reader)
     players = stats.top_scorers_by_nationality("FIN")
 
-    # players = stats.top_scorers_by_nationality("FIN")
-    #print("JSON-muotoinen vastaus:")
-    #print(response)
-    
-    # suomalaiset = []
-    
-#    # for player_dict in response:
-#     #    player = Player(
-#             player_dict['name'],
-#             player_dict['nationality'],
-#             player_dict['assists'],
-#             player_dict['goals'],
-#             player_dict['penalties'],
-#             player_dict['team'],
-#             player_dict['games'],
-            
-#             summa = player_dict['assists'] + player_dict['goals']
-        # )
 
-        # players.append(player)
-        #########
-        
-    #     if player_dict['nationality'] == "FIN":
-    #         suomalaiset.append(player)
-        
-    # print("Oliot:")
-    
-    # suomalaiset.sort(reverse=True)
-    # for player in suomalaiset:
-    #     summa =  player.goals + player.assists
-        
-    #     print(player.name, player.nationality,player.goals, " + ", player.assists, " = ", summa)
-        
     for player in players:
         print(player)
 
