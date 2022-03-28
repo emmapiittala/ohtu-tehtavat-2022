3ca53d6ed296c3d7dd043a979806ba0f1994d674
Emma Piittala <emma.piittala@gmail.com>
2022-03-28 21:28:41 +0300

    pushataan 2 tehtävä

--- viikko4/maksukortti-mock/src/tests/kassapaate_test.py
+++ viikko4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -23,3 +23,18 @@ class TestKassapaate(unittest.TestCase):
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_not_called()
+
+    def test_kortille_ei_ladata_rahaa_jos_summa_on_negatiivinen(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.maara = 0
+        
+        self.kassa.lataa(maksukortti_mock, -1)
+        
+        maksukortti_mock.lataa.assert_not_called()
+        
+    def test_kortille_lisataan_rahaa_jos_summa_on_positiivinen(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.maara = 5
+        
+        self.kassa.lataa(maksukortti_mock, 5)
+        maksukortti_mock.lataa.assert_called_with(5)
