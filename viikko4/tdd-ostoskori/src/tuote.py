26a48c4d3375b76ac7b81bf3ede95d7bb32de621
Emma Piittala <emma.piittala@gmail.com>
2022-03-29 15:15:31 +0300

    ostoskoori palauttaa oikean maaran

--- viikko4/tdd-ostoskori/src/tuote.py
+++ viikko4/tdd-ostoskori/src/tuote.py
@@ -1,14 +1,16 @@
 class Tuote:
-  def __init__(self, nimi: str, hinta: int):
-      self._nimi = nimi
-      self._hinta = hinta
-      self._saldo = 0
 
-  def hinta(self):
-    return self._hinta
 
-  def nimi(self):
-    return self._nimi
+    def __init__(self, nimi: str, hinta: int):
+        self._nimi = nimi
+        self._hinta = hinta
+        self._saldo = 0
 
-  def __repr__(self):
-      return f"{self._nimi} hinta {self._hinta} euroa"
+    def hinta(self):
+        return self._hinta
+
+    def nimi(self):
+        return self._nimi
+
+    def __repr__(self):
+        return f"{self._nimi} hinta {self._hinta} euroa"
