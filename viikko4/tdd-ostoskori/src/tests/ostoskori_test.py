1ba5e39cf68918192494f652e60f73998fcf23ac
Emma Piittala <emma.piittala@gmail.com>
2022-03-29 18:59:52 +0300

    vaihe 7

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -36,5 +36,10 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(self.viina)
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
         
- 
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_ostoskorin_hinta_on_sama_kuin_kaks_kertaa_tuotteen_hinta(self):
+        self.kori.lisaa_tuote(self.viina)
+        self.kori.lisaa_tuote(self.viina)
+        self.assertEqual(self.kori.hinta(), 14)
+        
+    
         
